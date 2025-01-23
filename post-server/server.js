3de325dffe2346c3be03e300ed04e9d055d5a72a
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-01-23 22:44:18 +0200

    server handling post request from html form page

--- post-server/server.js
+++ post-server/server.js
@@ -12,6 +12,24 @@ function get(res) {
 	res.end(form);
 }
 
+function post(req, res) {
+	if (req.headers['content-type'] !== 'application/x-www-form-urlencoded') {
+		error(res, 415);
+		return;
+	}
+
+	let input = '';
+
+	req.on('data', function (chunk) {
+		input += chunk.toString();
+	});
+
+	req.on('end', function () {
+		console.log(input);
+		res.end(http.STATUS_CODES[200]);
+	});
+}
+
 function error(res, code) {
 	res.statusCode = code;
 	res.end(http.STATUS_CODES[code]);
@@ -21,6 +39,11 @@ http
 	.createServer(function (req, res) {
 		if (req.method === 'GET') {
 			get(res);
+			return;
+		}
+
+		if (req.method === 'POST') {
+			post(req, res);
 			return;
 		}
 

92546197f4a7e2e923f185936a9b9699e4669eff
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-01-23 22:21:18 +0200

    server serving html form page to every get request hit

--- post-server/server.js
+++ post-server/server.js
@@ -1,1 +1,31 @@
 // a server that accepts and handles both HTTP GET and HTTP POST requests using the Node.js core APIs provided by the http module
+
+const http = require('node:http');
+const fs = require('node:fs');
+const path = require('node:path');
+
+const pathOfForm = path.resolve(process.cwd(), './puplic', 'form.html');
+const form = fs.readFileSync(pathOfForm);
+
+function get(res) {
+	res.writeHead(200, { 'Content-Type': 'text/html' });
+	res.end(form);
+}
+
+function error(res, code) {
+	res.statusCode = code;
+	res.end(http.STATUS_CODES[code]);
+}
+
+http
+	.createServer(function (req, res) {
+		if (req.method === 'GET') {
+			get(res);
+			return;
+		}
+
+		error(res, 405);
+	})
+	.listen('3000', function () {
+		console.log('Server running on port 3000...');
+	});

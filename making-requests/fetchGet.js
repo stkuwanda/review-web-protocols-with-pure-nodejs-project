d4e1f995a7f3e85ea8130d1c20d563e6e1b72767
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-01-23 15:44:56 +0200

    added more code to make sure response data can be written to a json file using NodeJs core fs APIs

--- making-requests/fetchGet.js
+++ making-requests/fetchGet.js
@@ -1,18 +1,36 @@
 // using the NodeJS core fetch API to make a get request to a target url
+
+const fs = require('node:fs');
+
 const url = 'https://api.github.com/orgs/nodejs';
 
-async function getWebResource(url) {
-  try {
-    const res = await fetch(url);
-    if(!res.ok){
-      throw new Error(`HTTP Error! Status code: ${res.status}`)
-    }
+function storeToJSONDataFile(data) {
+	return new Promise(function (resolve, reject) {
+		fs.writeFile('data2.json', data, function (err) {
+			if (err) {
+				reject(err);
+				return;
+			}
 
-    const data = await res.json();
-    console.log('GET request succeeded. Data:', data);
-  } catch (error) {
-    console.error('GET request errored:', error);
-  }
+			resolve('Data file creation is complete.');
+		});
+	});
+}
+
+async function getWebResource(url) {
+	try {
+		const res = await fetch(url);
+		if (!res.ok) {
+			throw new Error(`HTTP Error! Status code: ${res.status}`);
+		}
+
+		const data = await res.json();
+		const message = await storeToJSONDataFile(JSON.stringify(data, null, 2));
+		console.log(message);
+		console.log('GET request succeeded. Data:', data);
+	} catch (error) {
+		console.error('GET request errored:', error);
+	}
 }
 
 getWebResource(url);

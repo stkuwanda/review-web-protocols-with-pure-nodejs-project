881808ececd969906a83c59af6c966deb035e285
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-01-23 11:09:02 +0200

    added error handling, added passthrough stream to bridge to stdout and new html file

--- making-requests/requests.js
+++ making-requests/requests.js
@@ -1,8 +1,19 @@
 // sending HTTP requests using Nodejs core APIs
 
 const http = require('node:http');
+const fs = require('node:fs');
+const { PassThrough } = require('node:stream');
+
+const ws = fs.createWriteStream('./index.html');
+const bridge = new PassThrough();
 
 // send a request to the resource with the url
-http.get('http://example.com', function (res) {
-  res.pipe(process.stdout);
-});
+http
+	.get('http://example.com', function (res) {
+		res.pipe(bridge);
+		bridge.pipe(process.stdout);
+		bridge.pipe(ws);
+	})
+	.on('error', function (error) {
+		console.error('Something failed:', error);
+	});
